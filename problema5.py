c2c2c236c19b59f7ccf22de393afa99ad26e5d55
ricardoifc <ricardoifc@me.com>
2019-10-18 10:57:43 -0500

    python

--- problema5.py
+++ problema5.py
@@ -2,3 +2,17 @@
 	@ricardoifc
 	Problema5
 """
+
+edad = [10, 12, 13, 14, 16, 18, 20, 30, 31, 32, 33, 40, 50]
+
+black_edad = [10, 14, 30, 32, 40, 16]
+print()
+def comparacion(x):
+	black_edad
+	if x in black_edad:
+		return false
+	else:
+		return false
+
+resultado = filter(comparacion_edades, edades)
+print(list(resultado))
